18fbff92e9305fad1f75b6490f05f7d6a47fda12
conner reitmeier <conner@reitmeier.org>
2024-05-16 23:37:59 -0500

    build update

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,12 +1,10 @@
 const path = require('path')
-
 module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
       [
         'module-resolver',
         {
